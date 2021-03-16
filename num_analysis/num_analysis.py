eb467268c67822d1604697e9398c1b8931099014
Florian Trost <flter3177@gmail.com>
2021-03-16 16:20:14 +0100

    added results of first two tasks of numerical analysis to notebook

--- num_analysis/num_analysis.py
+++ num_analysis/num_analysis.py
@@ -2,22 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# read in data
 df = pd.read_csv('efield.t', delim_whitespace=True)
 
-x = df[df.columns].var() > 0.0001
+# plot data; first entire data set, then without time values
+df.plot()
+df.iloc[:,1:].plot()
 
+# delete columns 'x' and 'z'
+x = df[df.columns].var() > 0.0001
 df = df[df.columns[x]]
 
-#print(df)
-
+# calculate fft of column 'y'
 f = np.fft.fft(df['y'])
+f = f.real
 
-#print(f)
+print(f)
 
-#plt.figure()
-#df['y'].plot()
-#f.plot()
 
-plt.plot(f)
+#plt.show()
+plt.figure()
+plt.plot(f, label='FFT of y')
+plt.legend()
 
-plt.show()
+#plt.show()
+
+plt.savefig("fft.pdf")
