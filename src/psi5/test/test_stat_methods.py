e2b1063232a019d38ea5a323694ede555a8e82be
Patrizia <80414771+PatriziaVivien@users.noreply.github.com>
2021-03-30 13:11:01 +0200

    trying to add the right amount of whitespaces

--- src/psi5/test/test_stat_methods.py
+++ src/psi5/test/test_stat_methods.py
@@ -14,17 +14,16 @@ from psi5 import stat_methods as sm
 
 
 test_df = pd.DataFrame(
-            {'a' : [1, 2, 1.5],
-             'b' : [1, 2, 3],
-             'c' : [3, 2, 1]},
-             index = [1, 2, 3])
+                       {'a': [1, 2, 1.5],
+                        'b': [1, 2, 3],
+                        'c': [3, 2, 1]},
+                       index=[1, 2, 3])
 
 answer_df = pd.DataFrame(
-            {'level_0' : ['a', 'a', 'b'],
-             'level_1' : ['b', 'c', 'c'],
-             0 : [0.5, -0.5, -1.0]},
-            index = [0, 1, 2])
+                         {'level_0': ['a', 'a', 'b'],
+                          'level_1': ['b', 'c', 'c'],
+                          0: [0.5, -0.5, -1.0]},
+                         index=[0, 1, 2])
 
-#@pytest.mark
 def test_calc_pairwise_corr():
     pd.testing.assert_frame_equal(sm.calc_pairwise_corr(test_df), answer_df)
