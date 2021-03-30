2760ba4b3f32e944c7aeb63be52bef1a58819da2
FTrost <flter3177@gmail.com>
2021-03-30 13:24:16 +0200

    corrected num_methods.py for linter

--- src/psi5/num_methods.py
+++ src/psi5/num_methods.py
@@ -12,37 +12,36 @@ import pandas as pd
 
 
 def dft(signal: pd.Series, times: pd.Series):
-	""" Calculating the Direct Fourier Transform of a series
-		and the corresponding frequencies from its time stamps
+    """ Calculating the Direct Fourier Transform of a series
+        and the corresponding frequencies from its time stamps
 
-		Parameters:
-			signal (pandas.Series): Signal
-			times (pandas.Series): Time stamps of signal
+        Parameters:
+            signal (pandas.Series): Signal
+            times (pandas.Series): Time stamps of signal
 
-		Returns:
-			F: pandas.DataFrame
-	"""
-	freq = np.fft.fftfreq(times.size, times[1]-times[0])
-	amp = np.fft.fft(signal)
+        Returns:
+            F: pandas.DataFrame
+    """
+    freq = np.fft.fftfreq(times.size, times[1]-times[0])
+    amp = np.fft.fft(signal)
 
-	F = pd.DataFrame({ "freq": freq, "amp": amp })
+    F = pd.DataFrame({"freq" : freq, "amp" : amp})
 
-	return F
+    return F
 
 
 def acf(signal: pd.Series):
-	""" Calculating the (quasi) Autocorrelation Function of a series
+    """ Calculating the (quasi) Autocorrelation Function of a series
 
-		Parameters:
-			signal (pandas.Series): Signal
+        Parameters:
+            signal (pandas.Series): Signal
 
-		Returns:
-			A: int, float, complex, ...
-	"""
-	A = 0
+        Returns:
+            A: int, float, complex, ...
+    """
+    A = 0
 
-	for i in np.arange(0,signal.size,1):
-		A += signal[i]*signal[0]
+    for i in np.arange(0, signal.size, 1):
+        A += signal[i]*signal[0]
 
-	return A
-
+    return A
